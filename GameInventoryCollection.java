1d1e1bac28ead3144598a5716e59eccf955faff8
Vibhav Khare <122405274+vibhavkhare@users.noreply.github.com>
2023-10-30 20:15:16 -0700

    Main Collection Class

--- GameInventoryCollection.java
+++ GameInventoryCollection.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Assignment11 {
+public class Assignment11 { 
     public static void main(String[] args) {
         ZyBoxLiveStore store = new ZyBoxLiveStore();
         boolean quit = false;
